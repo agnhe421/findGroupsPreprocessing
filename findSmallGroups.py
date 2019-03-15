b1df5c5f5d6fea7fd60c0bb58b9b39fa5e660b81
Agnes <agnhe421@student.liu.se>
2019-03-15 20:01:58 +0100

    found small groups

--- findSmallGroups.py
+++ findSmallGroups.py
@@ -19,76 +19,56 @@ numberOfPeopleInGroupLIst = []
 print("Finding the group...")
 
 with open(dataPath) as f:
-	reader = csv.DictReader(f, fieldnames=['TimeStamp', 'id', 'type', 'X', 'Y']) 
+	reader = csv.DictReader(f, fieldnames=['TimeStamp', 'id', 'type', 'X', 'Y', 'GroupId', 'GroupSize']) 
 	for row in reader:
 		#Find the people who have checked in at the same place in the same time
 		if(row['type'] == 'check-in'):
 			data.append(row)
-			
-
-#MAKE SURE IT'S THE FIRST CHECK IN 
-	#kolla om id:t redan finns i listan av grupper 
-#FIND PEOPLE WHO CHECKED IN AT THE SAME TIME
-#DELETE EVERYBODY IN THE GROUP THAT'S NOT THE FIRST PERSON 
-#SAVE INFORMATION ABOUT HOW MANY PEOPLE IN THE GROUP AND CREATE A GROUP ID
-
+		
 
 for i in range(0, len(data)):
 	if not(data[i]['id'] in group):
 		#print('looking for groups...')
-
 		if(i+1 < len(data)):
 			if not(data[i]['X'] == data[i+1]['X'] and data[i]['Y'] == data[i+1]['Y'] and data[i]['TimeStamp'] == data[i+1]['TimeStamp'] ):
 				#print(data[i])
-				print('NEW member')
-				print(data[i])
+				#print('NEW member')
 				group.append(data[i])
 				numberOfPeopleIngroup = len(deletedMembers)
 				numberOfPeopleInGroupLIst.append(numberOfPeopleIngroup)
 				deletedMembers.clear()
+				data[i]['GroupSize'] = groupSize + 1
+				#print(data[i])
+				groupSize = 0
+
 			else:
 				deletedMembers.append(data[i])
-				print('member DELETED')
-				print(data[i])
+				groupSize = len(deletedMembers)
+				#print(groupSize); 
+				data[i]['GroupSize'] = groupSize + 1
+				#print('member DELETED')
+		#		print(data[i])
 		#print('next dataline')
 
 for x in range(0, len(group)):
-	print(group[x])
-for y in range (0, len(numberOfPeopleInGroupLIst)):
-	print(numberOfPeopleInGroupLIst[y])
+# 	print(group[x])
+	group[x]['GroupId'] = x
+	#print(group[x])
 
+	# with open(..., 'wb', newline='') as myfile:
+    #  wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    #  wr.writerow(mylist)
 
+	
+	with open('parsed/parsedDataFile', 'w') as csvfile:
+		writer = csv.writer(csvfile)
+		writer.writerow(group)
 
+#csvFile.close()
+	# 	for i in range(0, len(group)): 
+	# 		writer.writerow(group[i])
 
+	# 	for row in range(0, len(group)): 
+	# 		writer.writerow(group[row])
 
-
-	#print(data[i])
-			#elif(data[i]['X'] == data[i+1]['X'] and data[i]['Y'] == data[i+1]['Y'] and data[i]['TimeStamp'] == data[i+1]['TimeStamp'] ):
-
-# for i in range (0, len(data)): 
-# 	if(i+1 < len(data)):
-# 		#People who checked in at the same time position as at least one other person
-
-# 		if(data[i]['X'] == data[i+1]['X'] and data[i]['Y'] == data[i+1]['Y'] and ):#and data[i]['TimeStamp'] == data[i+1]['TimeStamp']):	
-# 			if(data[i]['TimeStamp'] == data[i+1]['TimeStamp']):
-# 				print('same timestamp')
-# 				# print(data[i])
-# 				#print('Same timestamp')
-# 				#print(data[i])
-# 				group.append(data[i])
-# 				#print(data[i])
-
-# 			else: 
-# 			 	print('NOT the same timestamp')
-# 			# 	print(data[i])
-
-# 		# if not(data[i]['X'] != data[i+1]['X'] or data[i]['Y'] != data[i+1]['Y'] or data[i]['TimeStamp'] != data[i+1]['TimeStamp']):
-# 		# 	group.append(data[i])
-# 		# 	#print(data[i])
-
-
-# for i in range(0, len(group)):
-# 	print(group[i])
-
-#print('Finished!')
-
+	# #csvfile.close()	
