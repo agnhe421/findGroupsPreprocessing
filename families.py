bba7367f64006e2e1deb9a1988b34ca0d27ed399
Agnes <agnhe421@student.liu.se>
2019-09-04 08:01:06 +0200

    find families script

--- families.py
+++ families.py
@@ -1,25 +1,49 @@
 import csv
 import os
 from collections import defaultdict
+from datetime import datetime
+import time
 dataPath = "/Users/agnesheppich/Documents/School/tnm098/findGroupsPreprocessing/parsed/friday.csv"
+printToFile = 'familiesFriday'
 families = []
 data = []
 print("Reading data...")
+
+#start = 8am, end = 12pm 
+fridayStart = 1402034400
+saturdayStart = 1402120800
+sundayStart = 1402207200
+
+fridayEnd = 1402048800
+saturdayEnd = 1402135200
+sundayEnd = 1402221600
 
 with open(dataPath) as f:
     reader = csv.DictReader(f, fieldnames=['TimeStamp', 'id', 'type', 'X', 'Y', 'GroupId', 'GroupSize']) 
     next(reader)   
     for row in reader:
-        if float(row['GroupSize']) > 1: 
-            families.append(row)
+        if float(row['GroupSize']) > 2 and float(row['GroupSize']) < 8: 
+            #people who only checked in in the kids area 
+            if (float(row['X'])  > 55 and float(row['Y']) > 60): 
+                #Look for people who checked into the park early and didn't stay late
+                d = datetime.strptime(row['TimeStamp'], '%Y-%m-%d %H:%M:%S')
+                timeStampInSeconds = time.mktime(d.timetuple())
+                if((timeStampInSeconds > fridayStart and timeStampInSeconds < fridayEnd) 
+                or (timeStampInSeconds > saturdayStart and timeStampInSeconds < saturdayEnd)
+                or (timeStampInSeconds > sundayStart and timeStampInSeconds < sundayEnd)): 
+                   families.append(row)
         
-print('Family? ')
-print(families)
 
-# with open('groupSizes/groupSizesSunday', 'w') as outfile:
-#     out = csv.DictWriter(outfile, fieldnames = ["alone", "twoToThree", "threeToFour", "moreThanFour"])
-#     out.writeheader()
-#     for test in data: 
-#         outfile.write(str(test))
-#         outfile.write(',')
-#     outfile.close()	
+numberOfPeopleVisitingInAFamily = 0; 
+for i in range(0, len(families)): 
+    print(families[i])
+    numberOfPeopleVisitingInAFamily = numberOfPeopleVisitingInAFamily + int(families[i]['GroupSize'])
+
+print(len(families))
+print(numberOfPeopleVisitingInAFamily)
+
+with open('parsed/' + printToFile, 'w') as outfile:
+    fp = csv.DictWriter(outfile, families[0].keys())
+    fp.writeheader()
+    fp.writerows(families)
+    outfile.close()	
