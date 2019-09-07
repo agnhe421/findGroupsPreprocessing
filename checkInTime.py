2d9175062bb1feca6d5b2c20eadd0c90e58d7718
Agnes <agnhe421@student.liu.se>
2019-09-07 12:29:51 +0200

    filter on time

--- checkInTime.py
+++ checkInTime.py
@@ -3,17 +3,32 @@ import os
 from collections import defaultdict
 from datetime import datetime
 import time
-# dataPath = "/Users/agnesheppich/Documents/School/tnm098/findGroupsPreprocessing/parsed/friday.csv"
-dataPath = "/Users/agnesheppich/Documents/School/tnm098/findGroupsPreprocessing/data/friday.csv"
-printToFile = 'earlyBirdsFriday'
+
+dataPath = "/Users/agnesheppich/Documents/School/tnm098/findGroupsPreprocessing/data/sunday.csv"
+printToFile = 'morningSunday'
 
 groups = []
 data = []
 print("Reading data...")
 
-earlyBirdsInFriday = 1402048800 #12pm
-earlyBirdsInSaturday = 1402135200
-earlyBirdsInSunday = 1402221600
+TwelvePmFriday = datetime.strptime('2014-6-06 12:00:00', '%Y-%m-%d %H:%M:%S')
+TwelvePmFridayInSeconds = time.mktime(TwelvePmFriday.timetuple())
+
+TwelvePmSaturday = datetime.strptime('2014-6-07 12:00:00', '%Y-%m-%d %H:%M:%S')
+TwelvePmSaturdayInSeconds = time.mktime(TwelvePmSaturday.timetuple())
+
+TwelvePmSunday = datetime.strptime('2014-6-08 12:00:00', '%Y-%m-%d %H:%M:%S')
+TwelvePmSundayInSeconds = time.mktime(TwelvePmSunday.timetuple())
+
+
+SixPmFriday = datetime.strptime('2014-6-06 18:00:00', '%Y-%m-%d %H:%M:%S')
+SixPmFridayInSeconds = time.mktime(SixPmFriday.timetuple())
+
+SixPmSaturday = datetime.strptime('2014-6-07 18:00:00', '%Y-%m-%d %H:%M:%S')
+SixPmSaturdayInSeconds = time.mktime(SixPmSaturday.timetuple())
+
+SixPmSunday = datetime.strptime('2014-6-08 18:00:00', '%Y-%m-%d %H:%M:%S')
+SixPmSundayInSeconds = time.mktime(SixPmSunday.timetuple())
 
 with open(dataPath) as f:
     reader = csv.DictReader(f, fieldnames=['TimeStamp', 'id', 'type', 'X', 'Y', 'GroupId']) 
@@ -22,13 +37,23 @@ with open(dataPath) as f:
         if(row['type'] == 'check-in'):
             d = datetime.strptime(row['TimeStamp'], '%Y-%m-%d %H:%M:%S')
             timeStampInSeconds = time.mktime(d.timetuple())
-            
-            if(
-                # (timeStampInSeconds > earlyBirdsInFriday)):
-                (timeStampInSeconds > earlyBirdsInSaturday)):
-                # (timeStampInSeconds > earclyCheckInSunday)): 
+            print('time in seconds: ' + str(timeStampInSeconds))
+            if (
+                # (timeStampInSeconds < TwelvePmFridayInSeconds)):
+                # (timeStampInSeconds < TwelvePmSaturdayInSeconds)):
+                (timeStampInSeconds < TwelvePmSundayInSeconds)): 
                 groups.append(row)
-        
+            # if (
+                # (timeStampInSeconds > TwelvePmFridayInSeconds and timeStampInSeconds < SixPmFridayInSeconds)):
+                # (timeStampInSeconds > TwelvePmSaturdayInSeconds and timeStampInSeconds < SixPmSaturdayInSeconds)):
+                # (timeStampInSeconds > TwelvePmSundayInSeconds and timeStampInSeconds < SixPmSundayInSeconds)):
+                # groups.append(row)
+            # if (
+                # (timeStampInSeconds > SixPmFridayInSeconds)):
+                # (timeStampInSeconds > SixPmSaturdayInSeconds)):
+                # (timeStampInSeconds > SixPmSundayInSeconds)):
+
+                # groups.append(row)
 
 numberOfPeople = 0
 individualIds = []
@@ -39,10 +64,10 @@ for i in range (0, len(groups)):
     ids.append(groups[i]['id'])
 ids = list(dict.fromkeys(ids))
 
-for j in range(0, len(ids)): 
-    print(ids[j])
+# for j in range(0, len(ids)): 
+    # print(ids[j])
 
-print(len(ids))
+print('total: ' + str(len(ids)))
 
 with open('parsed/' + printToFile, 'w') as outfile:
     fp = csv.DictWriter(outfile, groups[0].keys())
